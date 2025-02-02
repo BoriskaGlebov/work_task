121e4bd1fc3427b2a867a1fa3c6d3810e730ae3f
Boriska <boristheblade.glebov@yandex.ru>
2025-02-03 00:06:52 +0300

    cities

---
+++ lazy_ilya/work_for_ilia/management/commands/update_globus.py
@@ -0,0 +1,22 @@
+from django.core.management import BaseCommand
+from docx import Document
+
+from work_for_ilia.models import SomeDataFromSomeTables, SomeTables
+from work_for_ilia.utils.my_settings.disrs_for_app import ProjectSettings
+from work_for_ilia.utils.parser_word.my_parser import Parser
+
+
+class Command(BaseCommand):
+    """
+    Команда создания продуктов
+    """
+    help = 'Обновляет данные таблиц'
+
+    def handle(self, *args, **options):
+        self.stdout.write("Начинаю обновление")
+        start_numm = 123
+        s = Parser(ProjectSettings.tlg_dir, start_numm)
+        doc = Document('D:\SkillBox\work_task\lazy_ilya\work_for_ilia\\utils\\test_dir\globus.docx')
+        print(s.globus_parser(doc))
+        # self.stdout.write(f"Созадн продукт {product.name}")
+        self.stdout.write(self.style.SUCCESS("БД ОБновлена"))
