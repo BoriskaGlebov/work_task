c9d01bed53f642f80fc0a4c09c1829a5d41f347f
Boriska <boristheblade.glebov@yandex.ru>
2025-02-04 11:34:18 +0300

    добавил админ панель

--- lazy_ilya/work_for_ilia/admin.py
+++ lazy_ilya/work_for_ilia/admin.py
@@ -1,39 +1,65 @@
 from django.contrib import admin
-
 from work_for_ilia.models import Counter, SomeTables, SomeDataFromSomeTables
 
 
-# Register your models here.
 @admin.register(Counter)
 class CounterAdmin(admin.ModelAdmin):
-    list_display = ('processed_at', 'num_files')  # Поля для отображения в списке
-    list_filter = ('processed_at',)  # Фильтры для боковой панели
-    search_fields = ('num_files',)  # Поля для поиска
+    """
+    Админка для модели Counter.
 
-    # Настройка отображения времени
+    Поля:
+        list_display: Поля для отображения в списке.
+        list_filter: Фильтры для боковой панели.
+        search_fields: Поля для поиска.
+    """
+
+    list_display = ('processed_at', 'num_files')
+    list_filter = ('processed_at',)
+    search_fields = ('num_files',)
+
     def processed_at(self, obj):
-        return obj.processed_at.strftime("%d.%m.%Y %H:%M")  # Форматирование даты и времени
+        """Форматирует дату обработки."""
+        return obj.processed_at.strftime("%d.%m.%Y %H:%M")
 
-    processed_at.admin_order_field = 'processed_at'  # Позволяет сортировать по этому полю
-    processed_at.short_description = 'Дата обработки'  # Название колонки в админке
+    processed_at.admin_order_field = 'processed_at'
+    processed_at.short_description = 'Дата обработки'
 
 
 @admin.register(SomeTables)
 class SomeTablesAdmin(admin.ModelAdmin):
-    list_display = ('id', 'table_name', 'related_data_count')  # Поля для отображения в списке
+    """
+    Админка для модели SomeTables.
+
+    Поля:
+        list_display: Поля для отображения в списке.
+        list_display_links: Поля, по которым можно перейти к редактированию записи.
+        search_fields: Поля для поиска.
+        list_filter: Фильтры для боковой панели.
+    """
+
+    list_display = ('id', 'table_name', 'related_data_count')
     list_display_links = 'id', 'table_name'
-    search_fields = ('table_name',)  # Поля для поиска
-    list_filter = ('processed_at',)  # Фильтры для боковой панели
+    search_fields = ('table_name',)
+    list_filter = ('processed_at',)
 
     def related_data_count(self, obj):
         """Возвращает количество связанных записей в SomeDataFromSomeTables."""
         return SomeDataFromSomeTables.objects.filter(table_id=obj).count()
 
-    related_data_count.short_description = 'Количество записей'  # Название колонки в админке
+    related_data_count.short_description = 'Количество записей'
 
 
 @admin.register(SomeDataFromSomeTables)
 class SomeDataFromSomeTablesAdmin(admin.ModelAdmin):
-    list_display = ('id', 'location', 'name_organ', 'pseudonim','ip_address')  # Поля для отображения в списке
-    search_fields = ('location', 'name_organ','pseudonim')  # Поля для поиска
-    list_filter = ('processed_at', 'table_id')  # Фильтры для боковой панели
+    """
+    Админка для модели SomeDataFromSomeTables.
+
+    Поля:
+        list_display: Поля для отображения в списке.
+        search_fields: Поля для поиска.
+        list_filter: Фильтры для боковой панели.
+    """
+
+    list_display = ('id', 'location', 'name_organ', 'pseudonim', 'ip_address')
+    search_fields = ('location', 'name_organ', 'pseudonim')
+    list_filter = ('processed_at', 'table_id')
