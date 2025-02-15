4a7b1d03aade3a0cad03821fd130f8b9324829d9
Boriska <boristheblade.glebov@yandex.ru>
2025-02-15 20:15:02 +0300

    работатает создание обновление городов через форму

--- lazy_ilya/work_for_ilia/forms.py
+++ lazy_ilya/work_for_ilia/forms.py
@@ -1,31 +1,19 @@
 from django import forms
-from django.core.exceptions import ValidationError
-from django.db import models
+
 from django.db.models import Max
-from django.forms import CheckboxInput
+
 
 from work_for_ilia.models import SomeDataFromSomeTables
 from work_for_ilia.utils.my_settings.settings_for_app import logger
 
 
 class CitiesForm(forms.ModelForm):
-    # letters = forms.BooleanField(
-    #     required=False,
-    #     widget=CheckboxInput,
-    #     label="Письма"
-    # )
-    # writing = forms.BooleanField(
-    #     required=False,
-    #     widget=CheckboxInput,
-    #     label="Записи"
-    # )
-
     class Meta:
         model = SomeDataFromSomeTables
         fields = 'table_id', "dock_num", "location", "name_organ", "pseudonim", 'letters', "writing", "ip_address", "some_number", "work_timme"
         widgets = {
             'writing': forms.CheckboxInput(attrs={'class': 'hidden-checkbox', 'id': 'writing'}),
-            'letters': forms.CheckboxInput(attrs={'class': 'hidden-checkbox', 'id': 'writing'}),
+            'letters': forms.CheckboxInput(attrs={'class': 'hidden-checkbox', 'id': 'letters'}),
         }
 
     def __init__(self, *args, **kwargs):
@@ -40,18 +28,3 @@ class CitiesForm(forms.ModelForm):
                 self.fields["dock_num"].initial = last_dock_num + 1
         else:
             self.fields["dock_num"].initial = 1
-
-    # def clean_dock_num(self):
-    #     dock_num = self.cleaned_data['dock_num']
-    #     table_id = self.cleaned_data.get('table_id')  # Получаем table_id
-    #
-    #     # Проверяем, что table_id существует
-    #     if not table_id:
-    #         raise ValidationError("Выберите таблицу (table_id).")
-    #
-    #     # Проверяем уникальность dock_num для данной table_id
-    #     if SomeDataFromSomeTables.objects.filter(table_id=table_id, dock_num=dock_num).exists():
-    #         logger.error(f"Попытка создать запиьс в Раздел {table_id.table_name} - {dock_num} ")
-    #         raise ValidationError("Этот номер уже существует для данной таблицы.")
-    #
-    #     return dock_num
