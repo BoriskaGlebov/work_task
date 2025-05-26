6712dd931df4b4b724ebcc46629df01ba06ac443
BoriskaGlebov <boristheblade.glebov@yandex.ru>
2025-05-26 23:39:38 +0300

    Разработка формы обновления записей городов

--- lazy_ilya/work_for_ilia/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class WorkForIliaConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "work_for_ilia"
-    verbose_name = "Ленивый Илья"
