f23dc2ac8c411fa539b0bb76954f380073f027d6
Boriska <boristheblade.glebov@yandex.ru>
2025-05-27 22:17:58 +0300

    настройка на windows

--- lazy_ilya/frontendOLD/src/cities/js/update-form.js
+++
@@ -1,9 +0,0 @@
-import {AccordionUploader} from "./accordionUploader.js";
-import {toggleAccentClasses} from "./toggleAccent.js";
-import {CityFormHandler} from "./admin-update-cities.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-    toggleAccentClasses('a-admin','a-admin-mob');
-    new AccordionUploader("uploadForm", "fileInput", "server-error");
-    new  CityFormHandler("updateFormCities");
-});
