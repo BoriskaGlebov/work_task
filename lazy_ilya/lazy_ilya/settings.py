9d4dd8e4f21073498355629aaa972011a3bdc98f
BoriskaGlebov <boristheblade.glebov@yandex.ru>
2025-06-28 22:19:55 +0300

    Подготовка в deploy

--- lazy_ilya/lazy_ilya/settings.py
+++ lazy_ilya/lazy_ilya/settings.py
@@ -176,11 +176,11 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_COOKIE_AGE = 30 * 60  # Время жизни сессии в секундах (например, 30 минут)
 SESSION_SAVE_EVERY_REQUEST = True  # Обновляет таймер сессии при каждом запросе
 AUTH_USER_MODEL = "myauth.CustomUser"
-FORCE_SCRIPT_NAME = '/work_task'
+# FORCE_SCRIPT_NAME = '/work_task'
 CSRF_TRUSTED_ORIGINS = [
-    "https://vpn-boriska.ru",
-    # Можно добавить и другие домены, если нужно
+    "http://localhost:8000",
 ]
+
 
 
 if __name__ == "__main__":
