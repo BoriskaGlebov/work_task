a182df790cd1a867784103d626a5a9064e7aa21b
BoriskaGlebov <boristheblade.glebov@yandex.ru>
2025-07-30 21:40:07 +0300

    работа над обновлением номеров телефонов

--- lazy_ilya/frontend/src/cities/js/update-form.js
+++ lazy_ilya/frontend/src/cities/js/update-form.js
@@ -1,11 +1,11 @@
 import {AccordionUploader} from "./accordionUploader.js";
 import {toggleAccentClasses} from "./toggleAccent.js";
 import {CityFormHandler, DOFormHandler} from "./admin-update-cities.js";
-import {InlineGlobusAutocomplete} from "./globus-search.js";
+import Inputmask from "inputmask";
 
 document.addEventListener("DOMContentLoaded", () => {
     toggleAccentClasses('a-admin','a-admin-mob');
     new AccordionUploader("uploadForm", "fileInput", "server-error");
     new  CityFormHandler("updateFormCities");
-    new DOFormHandler("updateFormDO");
+    new DOFormHandler("updateFormDO",{Inputmask});
 });
